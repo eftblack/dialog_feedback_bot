295d61f13cdf119b51a631e503134075a3cd86af
Arina Stepanischeva <42976103+eftblack@users.noreply.github.com>
2019-10-23 23:04:33 +0700

    Add files via upload

---
+++ config/config.py
@@ -0,0 +1,14 @@
+config = {
+    "email_config": {
+        "host": "MAIL_HOST",
+        "port": "MAIL_PORT",
+        "login": "MAIL_LOGIN",
+        "password": "MAIL_PASSWORD",
+        "to_mail": False,
+        "to_chat": True
+    },
+    "bot_config": {
+        "endpoint": "endpoint",
+        "token": "token"
+    },
+}
